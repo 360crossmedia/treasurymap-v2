4932c53692691df5e201de3afb09b42948eb6f8d
SEGR02 <sebastian.gomz02@gmail.com>
2024-01-01 18:57:46 -0400

    Sebas: bug fixes deployment main branch

--- src/app/service/apiLogin.js
+++ src/app/service/apiLogin.js
@@ -2,15 +2,15 @@ import axios from "axios";
 import { url } from "./url";
 
 export const apiLogin = async (datos) => {
-  const headers = {
-    "Content-Type": "application/json; charset=utf-8",
-    Accept: "application/json",
-    Authorization: "Bearer TuTokenDeAutorización", // Ajusta esto según tus necesidades
-    "User-Agent": "TuUserAgent", // Puedes dejar esto en blanco o establecerlo según tus necesidades
-    // Agrega los headers CORS necesarios
-    "Access-Control-Allow-Origin": "*", // Ajusta según tu configuración de CORS
-    "Access-Control-Allow-Credentials": "true", // Ajusta según tu configuración de CORS
-  };
+  // const headers = {
+  //   "Content-Type": "application/json; charset=utf-8",
+  //   Accept: "application/json",
+  //   Authorization: "Bearer TuTokenDeAutorización", // Ajusta esto según tus necesidades
+  //   User-Agent: "TuUserAgent", // Puedes dejar esto en blanco o establecerlo según tus necesidades
+  //   // Agrega los headers CORS necesarios
+  //   Access-Control-Allow-Origin: "*", // Ajusta según tu configuración de CORS
+  //   "Access-Control-Allow-Credentials": "true", // Ajusta según tu configuración de CORS
+  // };
 
   let user = {
     email: datos.email,
@@ -19,7 +19,7 @@ export const apiLogin = async (datos) => {
   let userJson = JSON.stringify(user);
 
   return axios
-    .post(`${url}/api/v1/auth/login`, userJson, { headers })
+    .post(`${url}/api/v1/auth/login`, userJson)
     .then((res) => res)
     .catch((error) => console.log(error));
 };
