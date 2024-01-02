b9eb321b0502b8745e3c2a3048f695b6e939d30a
SEGR02 <sebastian.gomz02@gmail.com>
2024-01-02 12:17:13 -0400

    Sebas: ready el main

--- src/app/service/apiLogin.js
+++ src/app/service/apiLogin.js
@@ -2,24 +2,13 @@ import axios from "axios";
 import { url } from "./url";
 
 export const apiLogin = async (datos) => {
-  const headers = {
-    "Content-Type": "application/json",
-  };
-
   let user = {
     email: datos.email,
     password: datos.password,
   };
-  let userJson = JSON.stringify(user);
-
-  const config = {
-    crossDomain: true,
-    withCredentials: true,
-    // Otras configuraciones...
-  };
 
   return axios
-    .post(`${url}/api/v1/auth/login`, user, config)
+    .post(`${url}/api/v1/auth/login`, user)
     .then((response) => response)
     .catch((error) => console.error("Error:", error));
 };
