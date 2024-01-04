7c45117955307ae66631c42ac70635396b54b1b6
SEGR02 <sebastian.gomz02@gmail.com>
2024-01-04 00:08:52 -0400

    Sebas/Ready with url deploy request

--- src/app/service/apiUploadImage.js
+++ src/app/service/apiUploadImage.js
@@ -3,7 +3,7 @@ import { url } from "./url";
 
 export const apiUploadImage = async (img) => {
   return axios
-    .post(`http://localhost:8000/api/v1/images`, img, {
+    .post(`${url}/api/v1/images`, img, {
       headers: {
         "Content-Type": "multipart/form-data",
       },
