682b53695383a916ae4725b10d70122fe15a48d2
SEGR02 <sebastian.gomz02@gmail.com>
2024-05-12 18:24:48 -0400

    Feat from Sebas: /insights as a public route

--- src/app/helpers/auth.js
+++ src/app/helpers/auth.js
@@ -5,6 +5,7 @@ export const redirectIfNotAuthenticated = (userId, pathname, router) => {
     "/login",
     "/contactUs",
     "/article",
+    "/insights",
     (path) => path.startsWith("/linkupdate"),
     (path) => path.startsWith("/companyPage"),
     (path) => path.startsWith("/restorePassword"),
