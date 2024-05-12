7dea2da879422c62e750f08ea4dc540ce2434f04
SEGR02 <sebastian.gomz02@gmail.com>
2024-05-12 15:37:34 -0400

    Add a.link on insights with category and company page mediazone

--- src/app/components/InsightsCard.jsx
+++ src/app/components/InsightsCard.jsx
@@ -1,15 +1,17 @@
 import React from "react";
 import styles from "../styles/InsightsWithCategory.module.css";
-import { usePathname, useRouter } from "next/navigation";
+import { usePathname } from "next/navigation";
 import { truncateHtmlString } from "../utils";
 
 const InsightsCard = ({ publication }) => {
-  const router = useRouter();
   const pathname = usePathname();
 
-  const handleOnClick = (isVideo, publicationId) => {
-    if (isVideo) router.push(`/publication/video/${publicationId}`);
-    else router.push(`/publication/article/${publicationId}`);
+  const handleHref = (publication) => {
+    if (publication?.url) {
+      return `/publication/video/${publication?.id}`;
+    } else {
+      return `/publication/article/${publication?.id}`;
+    }
   };
   return (
     <div
@@ -21,12 +23,9 @@ const InsightsCard = ({ publication }) => {
       }
     >
       <div className={styles.cardData}>
-        <h5
-          onClick={() => handleOnClick(publication?.url, publication?.id)}
-          className={styles.title}
-        >
-          {publication?.title}
-        </h5>
+        <a className={styles.link} href={handleHref(publication)}>
+          <h5 className={styles.title}>{publication?.title}</h5>
+        </a>
         <p className={styles.description}>
           {pathname != "/insights"
             ? publication.url
@@ -44,13 +43,14 @@ const InsightsCard = ({ publication }) => {
           {publication?.url ? " Video" : " Article"}
         </p>
       </div>
-      <div
-        onClick={() => handleOnClick(publication?.url, publication?.id)}
-        className={styles.cardImageContainer}
-        style={{
-          backgroundImage: `url(${publication?.coverImage})`,
-        }}
-      ></div>
+      <a className={styles.link} href={handleHref(publication)}>
+        <div
+          className={styles.cardImageContainer}
+          style={{
+            backgroundImage: `url(${publication?.coverImage})`,
+          }}
+        ></div>
+      </a>
     </div>
   );
 };
