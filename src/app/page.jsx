6a7236b69087e13622a16acef2b35cf983b7e7d8
SEGR02 <sebastian.gomz02@gmail.com>
2025-01-12 00:31:31 -0500

    Feat from Sebas: Multiplayer map is ready

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -2,26 +2,49 @@
 import Navbar from "./components/Navbar";
 import Footer from "./components/Footer";
 import styles from "./styles/layout.module.css";
-
-// import TreasuryMap from "./components/map/TreasuryMap";
-// import "./components/map/TreasuryMap.css";
-
-import NewTreasuryMap from './components/newmap3/NewTreasuryMap3.jsx';
-import './components/newmap3/NewTreasuryMap3.css'
+import NewTreasuryMap from "./components/newmap3/NewTreasuryMap3";
+import MultiplayerMap from "./components/multiplayerMap/MultiplayerMap";
+import "./components/multiplayerMap/MultiplayerMap.css";
+import { useState } from "react";
 
 const Layout = () => {
+  const [isMultiplayerMap, setIsMultiplayerMap] = useState(false);
+  const [flipping, setFlipping] = useState(false);
+
+  const handleToggleMap = () => {
+    setFlipping(true);
+    setTimeout(() => {
+      setIsMultiplayerMap(!isMultiplayerMap);
+      setFlipping(false);
+    }, 600); // Duración de la animación (coincide con el CSS)
+  };
+
   return (
     <div
       className={styles.mainContainer}
       style={{ backgroundPosition: "bottom", paddingTop: 0 }}
     >
       <div className={styles.mainContainerMap}>
-        <Navbar buttonLabel={"Log In"} />
+        <Navbar
+          buttonLabel={"Log In"}
+          multiplayerMap={isMultiplayerMap}
+          set={setIsMultiplayerMap}
+          rotate={handleToggleMap}
+        />
       </div>
-    
-      {/* <TreasuryMap /> */}
 
-      <NewTreasuryMap />
+      <div
+        className={`${styles.flipContainer} ${flipping ? styles.flipping : ""}`}
+      >
+        <div className={styles.flipCard}>
+          <div className={styles.front}>
+            {isMultiplayerMap ? <MultiplayerMap /> : <NewTreasuryMap />}
+          </div>
+          <div className={styles.back}>
+            {!isMultiplayerMap ? <NewTreasuryMap /> : <MultiplayerMap />}
+          </div>
+        </div>
+      </div>
 
       <Footer />
     </div>
---
+++ src/app/service/apiGetMultiplayerMapData.js
@@ -0,0 +1,12 @@
+import axios from "axios";
+import { url } from "./url";
+
+export const apiGetMultiplayerMapData = async () => {
+  try {
+    const response = await axios.get(`${url}/api/v1/mapdata/multiplayerMap`);
+    console.log(response.data);
+    return response.data;
+  } catch (error) {
+    console.error("Error:", error);
+  }
+};
