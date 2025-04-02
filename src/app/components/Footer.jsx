3c40f777e31fecb9842bbaa5c6e100f1c181c4be
TreasuryMap <154644128+TreasuryMap@users.noreply.github.com>
2025-04-02 13:29:19 +0200

    Changed the size of the logo

--- src/app/components/Footer.jsx
+++ src/app/components/Footer.jsx
@@ -34,8 +34,8 @@ const Footer = () => {
           <Image 
             src={kantoxLogo}
             alt="Kantox"
-            width={100}
-            height={30}
+            width={50}
+            height={10}
             className={styles.bnpLogo}
           />          
         </div>
